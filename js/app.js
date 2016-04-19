c4f574f8cb97c5a703f97d2d59cb235ba5c77e87
Colin Aulds <caulds989@gmail.com>
2016-04-19 14:57:02 -0500

    animations complete

--- js/app.js
+++ js/app.js
@@ -8,35 +8,25 @@ $(document).ready(function () {
         $(".ryu-still").show();
     })
     .mousedown(function () {
-        // play hadouken sound
+        playHadouken();
         $(".ryu-ready").hide();
         $(".ryu-throwing").show();
-        $(".hadouken").show()
-;        // show hadouken and animate it to the right of the screen
+        $(".hadouken").finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function() {
+                $(this).hide();
+                $(this).css('left', '591px');
+            }
+        );
     })
     .mouseup(function () {
        $(".ryu-throwing").hide();
        $(".ryu-ready").show();
     });
 });
-
-/*$(".ryu-still").mousedown(function () {
- $(".ryu-still").hide();
- $(".ryu-throwing").show();
- $(".hadouken").show();
- });
-
- $(".ryu-throwing").mouseup(function () {
- $(".ryu-throwing").hide();
- $(".ryu-still").show();
- });
-
- $(".ryu-still").mouseover(function () {
- $(".ryu-still").hide();
- $(".ryu-ready").show();
- });
-
- $(".ryu-ready").mouseout(function () {
- $(".ryu-ready").hide();
- $(".ryu-still").show();
- });*/
+function playHadouken() {
+    $("#hadouken-sound")[0].volume = 0.5;
+    $("#hadouken-sound")[0].load();
+    $("#hadouken-sound")[0].play();
+}
