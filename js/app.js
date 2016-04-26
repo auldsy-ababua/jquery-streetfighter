1e59763809d0587869447a89d9ef586936cb2e66
Colin Aulds <caulds989@gmail.com>
2016-04-26 14:35:25 -0500

    completed work

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    var coolRyuActive = false;
     $('.ryu').mouseenter(function() {
-        $(".ryu-still").hide();
-        $(".ryu-ready").show();
+        if (coolRyuActive === false) {
+            $(".ryu-still").hide();
+            $(".ryu-ready").show();
+        }
     })
     .mouseleave(function() {
-        $(".ryu-ready").hide();
-        $(".ryu-still").show();
+        if (coolRyuActive === false) {
+            $(".ryu-ready").hide();
+            $(".ryu-still").show();
+        }
     })
     .mousedown(function () {
         playHadouken();
@@ -20,13 +25,39 @@ $(document).ready(function () {
             }
         );
     })
-    .mouseup(function () {
+    .mouseup(function() {
        $(".ryu-throwing").hide();
        $(".ryu-ready").show();
+    })
+    $('body').keydown(function(e) {
+        if (e.which === 88) {
+            coolRyuActive = true;
+            playSexySong();
+            $('.ryu-ready').hide();
+            $('.ryu-still').hide();
+            $('.hadouken').hide();
+            $('.ryu-cool').show();
+            
+        }
+    })
+    .keyup(function (e) {
+        if (e.which === 88) {
+            coolRyuActive = false;
+            stopSexySong();
+            $('.ryu-cool').hide();
+            $('.ryu-ready').show();
+        }
     });
 });
 function playHadouken() {
     $("#hadouken-sound")[0].volume = 0.5;
     $("#hadouken-sound")[0].load();
     $("#hadouken-sound")[0].play();
+};
+function playSexySong() {
+    $("#sexy-song")[0].play();
+};
+function stopSexySong() {
+    $('#sexy-song')[0].pause();
+    $('#sexy-song')[0].currentTime=0;
 }
